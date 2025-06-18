a900ea97bc72e15c56d096caa6eb4e824c521f81
student marketplace <madphonk99@gmail.com>
2025-06-18 17:51:24 +0600

    Update database.js

--- database.js
+++ database.js
@@ -26,3 +26,16 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model('Product', productSchema);
 
 module.exports = Product;
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Number, default: 0 },
+    imageUrl: { type: String }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
