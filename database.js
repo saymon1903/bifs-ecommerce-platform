5f1f411b234fb1dcbce162ab55b6ae3a9379ae96
student marketplace <madphonk99@gmail.com>
2025-06-19 11:55:42 +0600

    Update database.js

--- database.js
+++ database.js
@@ -38,4 +38,21 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 
-module.exports = Product;
+module.exports = const firebase = require('firebase/app');
+require('firebase/database');
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://bifs-e-commerce-platform-default-rtdb.firebaseio.com/",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+module.exports = database;
